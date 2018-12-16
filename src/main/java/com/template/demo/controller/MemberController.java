2d3f1848f20406bcc09063585f60bc821847614a
jixinshi <389606993@qq.com>
2018-12-17 07:55:32 +0800

    fix mysql and log

--- src/main/java/com/template/demo/controller/MemberController.java
+++ src/main/java/com/template/demo/controller/MemberController.java
@@ -41,22 +41,32 @@ public class MemberController {
     @Resource
     private TemplateMemberDao templateMemberDao;
 
+    /**
+     * 测试
+     */
+    @ApiOperation(value = "测试")
+    @GetMapping(value = "/test")
+    public ResultData test() {
+        return ResultData.success("test", "11");
+    }
+
 
     /**
      * 登陆
+     *
      * @param memberDto
      * @return
      */
     @ApiOperation(value = "登陆用户")
     @PostMapping(value = "/login/user")
-    public ResultData loginUser(@RequestBody @NotNull TemplateMemberDto memberDto){
+    public ResultData loginUser(@RequestBody @NotNull TemplateMemberDto memberDto) {
 
-        if (StringUtils.isEmpty(memberDto.getLoginName())){
-            return ResultData.error(Error.USER_NOT_LOGIN_NULL.code,Error.USER_NOT_LOGIN_NULL.description);
+        if (StringUtils.isEmpty(memberDto.getLoginName())) {
+            return ResultData.error(Error.USER_NOT_LOGIN_NULL.code, Error.USER_NOT_LOGIN_NULL.description);
         }
 
-        if (StringUtils.isEmpty(memberDto.getLoginPassword())){
-            return ResultData.error(Error.USER_NOT_PASSWORD_NULL.code,Error.USER_NOT_PASSWORD_NULL.description);
+        if (StringUtils.isEmpty(memberDto.getLoginPassword())) {
+            return ResultData.error(Error.USER_NOT_PASSWORD_NULL.code, Error.USER_NOT_PASSWORD_NULL.description);
         }
 
         // 使用 shiro 进行登录
@@ -69,82 +79,84 @@ public class MemberController {
         //获取token
         UsernamePasswordToken token = new UsernamePasswordToken(userName, password);
 
-        try{
+        try {
             subject.login(token);
-            LOGGER.info(userName+"登入系统");
-        }catch (UnknownAccountException e) {
-            LOGGER.error(userName+"账号不存在");
-            return ResultData.error(Error.USERNAME_NOT_EXIST.code,Error.USERNAME_NOT_EXIST.description);
-        }catch (AuthenticationException e){
-            LOGGER.error(userName+"密码错误");
-            return ResultData.error(Error.USER_PASSWORD_FAIL.code,Error.USER_PASSWORD_FAIL.description);
+            LOGGER.info(userName + "登入系统");
+        } catch (UnknownAccountException e) {
+            LOGGER.error(userName + "账号不存在");
+            return ResultData.error(Error.USERNAME_NOT_EXIST.code, Error.USERNAME_NOT_EXIST.description);
+        } catch (AuthenticationException e) {
+            LOGGER.error(userName + "密码错误");
+            return ResultData.error(Error.USER_PASSWORD_FAIL.code, Error.USER_PASSWORD_FAIL.description);
         }
-        return ResultData.success(Success.USER_LOGIN_SUCCESS.code,Success.USER_LOGIN_SUCCESS.description);
+        return ResultData.success(Success.USER_LOGIN_SUCCESS.code, Success.USER_LOGIN_SUCCESS.description);
     }
 
     /**
      * 注册
+     *
      * @param memberDto
      * @return
      */
     @ApiOperation(value = "添加用户")
     @PostMapping(value = "/add/user")
-    public ResultData addUser(@RequestBody @NotNull TemplateMemberDto memberDto){
+    public ResultData addUser(@RequestBody @NotNull TemplateMemberDto memberDto) {
 
-        if (StringUtils.isEmpty(memberDto.getLoginName())){
-            return ResultData.error(Error.USER_NOT_LOGIN_NULL.code,Error.USER_NOT_LOGIN_NULL.description);
+        if (StringUtils.isEmpty(memberDto.getLoginName())) {
+            return ResultData.error(Error.USER_NOT_LOGIN_NULL.code, Error.USER_NOT_LOGIN_NULL.description);
         }
 
-        if (StringUtils.isEmpty(memberDto.getLoginPassword())){
-            return ResultData.error(Error.USER_NOT_PASSWORD_NULL.code,Error.USER_NOT_PASSWORD_NULL.description);
+        if (StringUtils.isEmpty(memberDto.getLoginPassword())) {
+            return ResultData.error(Error.USER_NOT_PASSWORD_NULL.code, Error.USER_NOT_PASSWORD_NULL.description);
         }
 
         TemplateMember member = templateMemberDao.selectOne(TemplateMemberCriteria.loginNameEqualTo(memberDto.getLoginName()).setLimit(1L));
-        if (member != null){
-            return ResultData.error(Error.USERNAME_EXIST.code,Error.USERNAME_EXIST.description);
+        if (member != null) {
+            return ResultData.error(Error.USERNAME_EXIST.code, Error.USERNAME_EXIST.description);
         }
         //加密
         String loginPassword = memberDto.getLoginPassword();
-        loginPassword = DigestUtils.Md5(memberDto.getLoginName(),loginPassword);
+        loginPassword = DigestUtils.Md5(memberDto.getLoginName(), loginPassword);
         memberDto.setLoginPassword(loginPassword);
         memberDto.setCreateTime(new Date());
         memberDto.setUpdateTime(new Date());
 
         int result = templateMemberDao.insert(memberDto);
-        if(result == 0 ){
-            return ResultData.error(Error.USER_REGISTER_FAIL.code,Error.USER_REGISTER_FAIL.description);
+        if (result == 0) {
+            return ResultData.error(Error.USER_REGISTER_FAIL.code, Error.USER_REGISTER_FAIL.description);
         }
 
-        return ResultData.success(Success.USER_REGISTER_SUCCESS.code,Success.USER_REGISTER_SUCCESS.description);
+        return ResultData.success(Success.USER_REGISTER_SUCCESS.code, Success.USER_REGISTER_SUCCESS.description);
     }
 
     /**
      * 修改
+     *
      * @param memberDto
      * @return
      */
     @ApiOperation(value = "修改用户")
     @PutMapping(value = "/motify/user")
-    public String motifyUser(@RequestBody @NotNull TemplateMemberDto memberDto){
+    public String motifyUser(@RequestBody @NotNull TemplateMemberDto memberDto) {
 
-        if (StringUtils.isEmpty(memberDto.getLoginName())){
+        if (StringUtils.isEmpty(memberDto.getLoginName())) {
             return "login name is empty!";
         }
 
-        if (StringUtils.isEmpty(memberDto.getLoginPassword())){
+        if (StringUtils.isEmpty(memberDto.getLoginPassword())) {
             return "login password is empty!";
         }
 
-        if (StringUtils.isEmpty(memberDto.getNewPassword())){
+        if (StringUtils.isEmpty(memberDto.getNewPassword())) {
             return "new password is empty!";
         }
 
         TemplateMember member = templateMemberDao.selectOne(TemplateMemberCriteria.loginNameEqualTo(memberDto.getLoginName()).setLimit(1L));
-        if (member == null){
+        if (member == null) {
             return "login name is not exist!";
         }
 
-        if (!member.getLoginPassword().equals(memberDto.getLoginName())){
+        if (!member.getLoginPassword().equals(memberDto.getLoginName())) {
             return "original password is wrong!";
         }
 
